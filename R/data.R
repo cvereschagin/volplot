b32c96475bbbbdd55d63338ab510b45742bc2e63
Carter Vereschagin <carter.vereschagin@gmail.com>
2026-01-16 12:13:28 -0700

    added datasets"

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Stock prices dataset
 #'
-#' @description Adjusted stock prices for Citigroup and General Motors.
+#' @description Adjusted stock prices for SPY
 #'
 #' @format A tibble with 3 variables:
 #' \describe{
@@ -10,3 +10,18 @@
 #' }
 #' @source Yahoo Finance via tidyquant
 "stocks"
+
+
+
+#' Stock prices dataset for 2 stocks
+#'
+#' @description Adjusted stock prices for SPY and AAPL
+#'
+#' @format A tibble with 3 variables:
+#' \describe{
+#'   \item{date}{Trading date}
+#'   \item{series}{Stock ticker symbol}
+#'   \item{value}{Adjusted closing price}
+#' }
+#' @source Yahoo Finance via tidyquant
+"stocks_2"
